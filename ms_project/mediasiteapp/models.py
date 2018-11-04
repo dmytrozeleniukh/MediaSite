3b1c26ce515081f89eb618ebdea50dfd795ad3a9
dmytrozeleniukh <d.zeleniukh@gmail.com>
2018-11-04 13:26:57 +0200

    staticfile

--- ms_project/mediasiteapp/models.py
+++ ms_project/mediasiteapp/models.py
@@ -14,12 +14,17 @@ class Category(models.Model):
 	def __unicode__(self):
 		return self.name
 
+def generate_filename(instance, filename):
+	filename = instance.slug + '.jpg'
+	return "{0}/{1}".format(instance, filename)
+
+
 class Article(models.Model):
 
 	category = models.ForeignKey(Category)
 	title = models.CharField(max_length=120)
 	slug = models.SlugField()
-	image = models.ImageField()
+	image = models.ImageField(upload_to = generate_filename)
 	content = models.TextField()
 	likes = models.PositiveIntegerField(default=0)
 	dislikes = models.PositiveIntegerField(default=0)
